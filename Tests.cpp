8a8788305afe9d6142af87d8047d611a67593f40
Grayson Spidle <graysonkc@gmail.com>
2022-03-16 15:10:55 -0500

    Updated Tests.cpp to include the performance tests

--- Tests.cpp
+++ Tests.cpp
@@ -1,20 +1,38 @@
-#define BASE256UMATH_SUPPRESS_TRUNCATED_CODE 0
-#define BASE256UMATH_FAST_OPERATORS 1
 #include "Base256uMath.h"
 #include "UnitTests.h"
-#ifdef __NVCC__
+#include "PerformanceTests.h"
+#ifdef __CUDACC__
 #include "CUDAUnitTests.h"
 #endif
 #include <iostream>
+#include <string>
+
+#ifndef MIN(a,b)
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#endif
+
+constexpr std::size_t KB = 1024;
+constexpr std::size_t MB = 1024 * KB;
+constexpr std::size_t GB = 1024 * MB;
 
 int main() {
 #ifndef _DEBUG
 	std::cout << "Make sure you're in debug mode to perform tests!" << std::endl;
 #endif
 
+#ifdef BASE256UMATH_FAST_OPERATORS
+	std::cout << "FAST_OPERATORS enabled" << std::endl;
+#endif
+	std::cout << "Running unit tests..." << std::endl;
 	Base256uMathTests::test_unit_tests();
-#ifdef __NVCC__
+#ifdef __CUDACC__
+	std::cout << "CUDA detected, running CUDA unit tests..." << std::endl;
 	Base256uMathTests::CUDA::test_unit_tests();
 #endif
+
+	std::cout << "All unit tests passed." << std::endl << std::endl;
+	std::cout << "Running performance tests..." << std::endl;
+	Base256uMathTests::Performance::test(100, 1 * MB);
+	std::cout << "Performance tests concluded." << std::endl;
 	return 0;
 }
